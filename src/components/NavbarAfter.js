8e8d3254e4b36c8684b59bd6a11b20e5f923a62f
ClaudyoPasqal <102457882+ClaudyoPasqal@users.noreply.github.com>
2022-06-14 16:44:59 +0700

    Update NavbarAfter.js

--- src/components/NavbarAfter.js
+++ src/components/NavbarAfter.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import NavbarAfterCss from './css/NavbarAfter.module.css';
 
 
 function NavbarAfter(){
+    
+    let navigate = useNavigate("")
+    const logout = async (e) =>{
+        e.preventDefault()
+        const { user, error } = await kontenbase.auth.logout()
+        navigate('/')
+        console.log(user, error)
+    }
     return(
         <div className={NavbarAfterCss.container}>
             <div className={NavbarAfterCss.navbar}>
@@ -22,7 +30,7 @@ function NavbarAfter(){
                 <div className={NavbarAfterCss.menu}>
                     <ul>
                         <li><img src="../images/user 2.png" alt="" /><Link to="/">Profile</Link></li>
-                        <li className={NavbarAfterCss.border}><img src="../images/logout 1.png" alt="" />Logout</li>
+                        <li className={NavbarAfterCss.border} onClick={logout}><img src="../images/logout 1.png" alt="" />Logout</li>
                     </ul>
                 </div>
             </div>
